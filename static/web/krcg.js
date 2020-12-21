58f9868b80b27eabb090f2b291387719b3569d5a
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2020-12-21 18:35:28 +0100

    Improve the website

--- static/web/krcg.js
+++ static/web/krcg.js
@@ -24,12 +24,13 @@ function nameToImage(text) {
         text = text.substr(4, text.length) + "the"
     }
     text = text
+        .replace(/™/g, "tm")
         .replace(/\s|,|\.|-|—|'|:|\(|\)|"|\/| |!/g, "")
-        .replace(/ö|ó/g, "o")
+        .replace(/ö|ó|ø/g, "o")
         .replace(/é|ë|è/g, "e")
         .replace(/œ/g, "oe")
         .replace(/ç/g, "c")
-        .replace(/á|ã/g, "a")
+        .replace(/á|ã|å/g, "a")
         .replace(/í|î/g, "i")
         .replace(/ñ/g, "n")
         .replace(/ü|ú/g, "u")
