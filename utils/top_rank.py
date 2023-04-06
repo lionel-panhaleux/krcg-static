23df110e403205f02f19b185a875a7c148b35b3b
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2023-04-06 15:42:19 +0200

    Top rank script

--- utils/top_rank.py
+++ utils/top_rank.py
@@ -3,11 +3,12 @@ from typing import Generator, Optional
 import aiohttp
 import asyncio
 import collections
+import csv
+import dataclasses
 import datetime
 import dateutil.relativedelta
 import enum
 import itertools
-import json
 import logging
 import os
 import pydantic
@@ -21,135 +22,82 @@ logger = logging.getLogger()
 VEKN_LOGIN = os.getenv("VEKN_LOGIN")
 VEKN_PASSWORD = os.getenv("VEKN_PASSWORD")
 
-IGNORE_ONLINE = False
-REFRESH = False
-MAX_EVENT_ID = 10680
-ONLINE_EVENTS = {
-    9611,
-    9612,
-    9614,
-    9619,
-    9622,
-    9630,
-    9633,
-    9636,
-    9648,
-    9654,
-    9657,
-    9661,
-    9664,
-    9666,
-    9694,
-    9706,
-    9710,
-    9713,
-    9714,
-    9719,
-    9720,
-    9721,
-    9723,
-    9724,
-    9740,
-    9743,
-    9745,
-    9753,
-    9754,
-    9755,
-    9759,
-    9760,
-    9761,
-    9762,
-    9763,
-    9767,
-    9768,
-    9769,
-    9770,
-    9771,
-    9772,
-    9773,
-    9774,
-    9775,
-    9777,
-    9780,
-    9782,
-    9783,
-    9784,
-    9788,
-    9790,
-    9794,
-    9802,
-    9810,
-    9813,
-    9815,
-    9828,
-    9831,
-    9832,
-    9834,
-    9837,
-    9842,
-    9852,
-    9856,
-    9861,
-    9872,
-    9879,
-    9885,
-    9888,
-    9891,
-    9897,
-    9898,
-    9899,
-    9930,
-    9931,
-    9932,
-    9946,
-    9960,
-    9976,
-    9984,
-    9996,
-    9997,
-    10008,
-    10012,
-    10021,
-    10044,
-    10046,
-    10047,
-    10058,
-    10064,
-    10068,
-    10073,
-    10077,
-    10099,
-    10190,
-    10209,
-    10232,
-    10240,
-    10273,
-    10285,
-    10455,
-    10458,
-    10476,
-    10508,
-    10522,
-    10527,
-    10528,
-    10576,
-    10591,
-}
 
-ONLINE_VENUES = {
-    (3845, "Online"),
-    (3876, "Campina Grande Online"),
-    (3866, "Lackey Online"),
-    (3839, "Lackey"),
-    (3853, "Lackey"),
-    (3859, "Lackey"),
-    (3905, "Lackey"),
-    (3856, "Lackey "),
-    (3867, "Lackey via Discord"),
-    (3842, "Lackey via Discord"),
-    (3858, "Lackey via Discord"),
-    (3841, "SchreckNET"),
-    (3908, "Table Top Simulator"),
+class RankingType(enum.IntEnum):
+    OFFLINE_CONSTRUCTED = 1
+    ONLINE_CONSTRUCTED = 2
+
+
+RANKING_TYPE = RankingType.OFFLINE_CONSTRUCTED
+REFRESH = False
+MAX_EVENT_ID = 10700
+
+# liga RTPs (manual addition from Igor)
+MANUAL_ADDITION = {
+    datetime.date(2020, 11, 28): {
+        "1004065": 158,
+        "6060012": 150,
+        "8500048": 176,
+        "3040004": 148,
+        "2150015": 311,
+        "1002979": 55,
+        "7400050": 9,
+        "1003214": 65,
+        "4550009": 17,
+        "1800007": 13,
+        "5230002": 17,
+        "7400007": 31,
+        "1800008": 9,
+        "6060015": 71,
+        "1003217": 53,
+        "1005186": 71,
+        "4440049": 31,
+        "3040009": 57,
+        "3540410": 25,
+        "5950005": 17,
+        "7400021": 5,
+        "7630001": 49,
+        "1002308": 29,
+        "6110002": 17,
+        "8580039": 31,
+        "5810002": 13,
+        "1820002": 71,
+        "1000565": 7,
+        "4200008": 15,
+        "1820011": 5,
+        "5850007": 37,
+        "6060020": 5,
+        "6060009": 5,
+        "6110000": 11,
+        "2570010": 35,
+        "4550004": 29,
+        "1003221": 35,
+        "1000317": 23,
+        "3200246": 35,
+        "5940017": 19,
+        "6060005": 25,
+        "7630037": 11,
+        "9670004": 33,
+        "1006065": 35,
+        "5110005": 5,
+        "6010157": 73,
+        "9098134": 5,
+        "6010151": 33,
+        "7060055": 43,
+        "7240009": 31,
+        "9098137": 33,
+        "6230002": 25,
+        "8500050": 5,
+        "3080036": 9,
+        "9098159": 27,
+        "3080035": 5,
+        "3190087": 13,
+        "1004066": 25,
+        "9200002": 5,
+        "7060071": 23,
+        "1004064": 57,
+        "9098136": 15,
+    }
 }
 
 
@@ -193,6 +141,7 @@ class Event(pydantic.BaseModel):
     # event_starttime: Optional[datetime.time] -- ignore because bad 24:00:00 format
     event_enddate: datetime.date
     # event_endtime: Optional[datetime.time] -- ignore because bad 24:00:00 format
+    event_isonline: bool
     venue_id: Optional[int]
     venue_name: Optional[str]
     venue_city: Optional[str]
@@ -206,6 +155,26 @@ class Event(pydantic.BaseModel):
     @property
     def date(self):
         return self.event_enddate or self.event_startdate
+
+    @property
+    def constructed(self):
+        return (
+            self.eventtype_id
+            in {
+                EventType.STANDARD_CONSTRUCTED,
+                EventType.CONTINENTAL_CHAMPIONSHIP,
+                EventType.CONTINENTAL_QUALIFIER,
+                EventType.NATIONAL_CHAMPIONSHIP,
+                EventType.NATIONAL_QUALIFIER,
+                EventType.MINI_QUALIFIER,
+                EventType.GRAND_PRIX,
+            }
+            and self.attendance > 9
+        )
+
+    @property
+    def online(self):
+        return self.event_isonline
 
 
 class Events(pydantic.BaseModel):
@@ -313,65 +282,122 @@ async def _get_vekns(vekns) -> dict:
                 result = result[0]
                 if result["veknid"] != vekn:
                     raise RuntimeError("VEKN ID# not found")
-                players[vekn] = result["firstname"] + " " + result["lastname"]
+                players[vekn] = (
+                    result["firstname"] + " " + result["lastname"],
+                    result["countryname"],
+                )
     return players
+
+
+@dataclasses.dataclass
+class Performance:
+    event_id: str
+    rtp: int
+
+    def __repr__(self):
+        return f"<{self.event_id}: {self.rtp}>"
+
+
+class Ranking:
+    window = dateutil.relativedelta.relativedelta(months=+18)
+    out_of_window = dateutil.relativedelta.relativedelta(months=+18, days=+1)
+
+    def __init__(self):
+        self.events: list[Event] = []
+        self.performances: dict[str, list[Performance]] = collections.defaultdict(list)
+        self.current: list[tuple[datetime.date, list[Event]]] = []
+
+    def rank(self):
+        return sorted(
+            [
+                (vekn, sorted(performance, reverse=True, key=lambda p: p.rtp)[:8])
+                for vekn, performance in self.performances.items()
+            ],
+            key=lambda a: sum(p.rtp for p in a[1]),
+            reverse=True,
+        )
+
+    def adjust_for_date(self, date: datetime.date):
+        while self.current and self.current[0][0] + self.window < date:
+            for event in self.current[0][1]:
+                logger.debug("Removing %s event#%s", event.date, event.event_id)
+                for player in event.players:
+                    self.performances[player.veknid].pop(0)
+            yield self.current[0][0] + self.out_of_window, self.rank()
+            self.current.pop(0)
+
+    def add(self, date: datetime.date, events: list[Event]):
+        for event in events:
+            logger.debug("Adding %s event#%s", event.date, event.event_id)
+            for player in event.players:
+                self.performances[player.veknid].append(
+                    Performance(event_id=event.event_id, rtp=player.rtp)
+                )
+        self.current.append((date, events))
+        return date, self.rank()
+
+    @classmethod
+    def load_from_vekn(cls):
+        ret = cls()
+        ret.events = sorted(asyncio.run(_get_events()), key=lambda e: e.date)
+        with open("tournament_points.json", "w") as fp:
+            fp.write(Events(__root__=ret.events).json())
+        return ret
+
+    @classmethod
+    def load_from_cache(cls):
+        ret = cls()
+        ret.events = Events.parse_file("tournament_points.json").__root__
+        return ret
+
+    def filter(self, type: RankingType):
+        if type == RankingType.OFFLINE_CONSTRUCTED:
+            self.events = [e for e in self.events if e.constructed and not e.online]
+        elif type == RankingType.ONLINE_CONSTRUCTED:
+            self.events = [e for e in self.events if e.constructed and e.online]
+
+    def yield_rankings(self):
+        for date, events_on_date in itertools.groupby(
+            self.events, key=lambda e: e.date
+        ):
+            events_on_date = list(events_on_date)
+            yield from self.adjust_for_date(date)
+            yield self.add(date, events_on_date)
+        yield from self.adjust_for_date(datetime.date.today())
 
 
 def main():
     if REFRESH:
-        events = asyncio.run(_get_events())
-        events = Events(__root__=sorted(events, key=lambda e: e.date))
-        with open("tournament_points.json", "w") as fp:
-            fp.write(events.json())
+        ranking = Ranking.load_from_vekn()
     else:
-        events = Events.parse_file("tournament_points.json")
+        ranking = Ranking.load_from_cache()
 
-    months_18 = dateutil.relativedelta.relativedelta(months=+18)
-    players_scores = collections.defaultdict(list)
+    ranking.filter(RANKING_TYPE)
+    if RANKING_TYPE == RankingType.ONLINE_CONSTRUCTED:
+        for date, players in MANUAL_ADDITION.items():
+            event = Event.construct(
+                event_id=f"manual-{date}",
+                event_name=f"manual-{date}",
+                event_enddate=date,
+                event_isonline=True,
+                attendance=len(players),
+                players=[
+                    PlayerResult.construct(
+                        veknid=veknid,
+                        rtp=rtp,
+                    )
+                    for veknid, rtp in players.items()
+                ],
+            )
+            logging.debug("Adding manual event: %s", event)
+            ranking.events.append(event)
+        ranking.events.sort(key=lambda e: e.date)
     top_players = []
     points_records = []
     top_player, top_score, max_score, top_start_date = None, 0, 0, None
-    events = [
-        e
-        for e in events.__root__
-        if e.eventtype_id
-        in {
-            EventType.STANDARD_CONSTRUCTED,
-            EventType.CONTINENTAL_CHAMPIONSHIP,
-            EventType.CONTINENTAL_QUALIFIER,
-            EventType.NATIONAL_CHAMPIONSHIP,
-            EventType.NATIONAL_QUALIFIER,
-            EventType.MINI_QUALIFIER,
-            EventType.GRAND_PRIX,
-        }
-        and datetime.date(2020, 1, 1) <= e.event_startdate <= datetime.date.today()
-        and int(e.event_id) in ONLINE_EVENTS
-        and e.attendance > 9
-    ]
-    past = itertools.groupby(events, key=lambda e: e.date)
-    current = itertools.groupby(events, key=lambda e: e.date)
-    begin_date, begin_events = next(past)
-    for date, events_on_date in current:
-        top_start_date = top_start_date or date
-        for event in events_on_date:
-            logger.debug("Adding %s event#%s", date, event.event_id)
-            for player in event.players:
-                players_scores[player.veknid].append(player.rtp)
-        while begin_date + months_18 < date:
-            for event in begin_events:
-                logger.debug("Removing %s event#%s", date, event.event_id)
-                for player in event.players:
-                    players_scores[player.veknid].pop(0)
-            begin_date, begin_events = next(past)
-        if not players_scores:
-            continue
-        new_top_player, new_top_score = max(
-            [
-                (vekn, sum(sorted(points, reverse=True)[:8]))
-                for vekn, points in players_scores.items()
-            ],
-            key=lambda a: a[1],
-        )
+    for date, rank in ranking.yield_rankings():
+        new_top_player, new_top_performance = rank[0]
+        new_top_score = sum(p.rtp for p in new_top_performance)
         if new_top_player != top_player:
             if top_player:
                 print(
@@ -388,28 +414,33 @@ def main():
             print(f"New max score on {date}: {top_player} with {max_score}")
             points_records.append([top_player, date, max_score])
 
-    print(f"{top_player} from {date} until today reached {top_score}")
-    top_players.append([top_player, top_start_date, datetime.date.today(), top_score])
-    print(f"{len(events)} tournaments taken into account (VEKN)")
-    players = asyncio.run(_get_vekns({p[0] for p in top_players}))
-    print("\n\n===================================================================\n")
-    print("TOP PLAYERS")
-    for vekn, start, end, score in top_players:
-        print(
-            ",".join(
-                [
-                    players.get(vekn, "N/A"),
-                    vekn,
-                    start.isoformat(),
-                    end.isoformat(),
-                    str(score),
-                ]
-            )
+    print(f"{len(ranking.events)} tournaments taken into account (VEKN)")
+    top50 = ranking.rank()[:50]
+    players = asyncio.run(
+        _get_vekns({p[0] for p in top_players} | {p[0] for p in top50})
+    )
+    with open("top_players.csv", "w") as fp:
+        w = csv.writer(fp)
+        w.writerows(
+            [
+                players.get(vekn, "N/A")[0],
+                vekn,
+                players.get(vekn, "N/A")[1],
+                start.isoformat(),
+                end.isoformat(),
+                str(score),
+            ]
+            for vekn, start, end, score in top_players
         )
-    print("\n\n===================================================================\n")
-    print("SCORE RECORDS")
-    for vekn, dat, score in points_records:
-        print(",".join([players.get(vekn, "N/A"), vekn, dat.isoformat(), str(score)]))
+    with open("score_records.csv", "w") as fp:
+        w = csv.writer(fp)
+        w.writerows(
+            [players.get(vekn, "N/A"), vekn, dat.isoformat(), str(score)]
+            for vekn, dat, score in points_records
+        )
+    print("Current ranking")
+    for player, score in top50:
+        print(player, players.get(player, "N/A"), sum(s.rtp for s in score), score)
 
 
 if __name__ == "__main__":
