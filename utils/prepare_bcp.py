f77271cd90ab093be6b624c584bd80fb2624ad54
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2023-04-17 12:14:40 +0200

    Fix format

--- utils/prepare_bcp.py
+++ utils/prepare_bcp.py
@@ -7,7 +7,7 @@ from krcg.parser import _CLAN
 
 
 def prepare_bcp(path):
-    for (dirpath, _dirnames, filenames) in os.walk(path):
+    for dirpath, _dirnames, filenames in os.walk(path):
         for name in filenames:
             clan_prefix = re.match(r"({})_".format(_CLAN), name.lower())
             if clan_prefix:
