697b3bd274709dac4726a2be37f0583125bd4510
Lionel Panhaleux <lionel.panhaleux@gmail.com>
2026-07-15 00:50:23 +0200

    actually keep https as default

--- static/web/krcg.js
+++ static/web/krcg.js
@@ -51,7 +51,7 @@ function nameToImage(text) {
         .replace(/[ǹńñṅňŋɲṇņṋṉŉƞȵ]/g, "n")
         .replace(/[ùúûũūŭüủůűǔȕȗưụṳųṷṵṹṻǜǘǖǚừứữửựʉ]/g, "u")
         .replace(/[źẑżžȥẓẕƶɀⱬ]/g, "z")
-    return "http://static.krcg.org/card/".concat(text, ".webp")
+    return "https://static.krcg.org/card/".concat(text, ".webp")
 }
 
 function krcgCards() {
